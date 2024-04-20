c1bb021482b71da2cd6e865f66db48a69072d8e3
Eltee <lytaiwo@hotmail.com>
2024-04-20 03:41:07 -0500

    Added ability to do Building actions.
    Added controller zoom support using triggers.

--- Assets/Xbox/GameplayControllerHandler.cs
+++ Assets/Xbox/GameplayControllerHandler.cs
@@ -1,6 +1,8 @@
 using System.Linq;
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace Assets.Xbox
 {
@@ -8,13 +10,20 @@ namespace Assets.Xbox
     {
         public static GameplayControllerHandler Instance { get; private set; }
 
-        private GameManager GameManager => GameManager.Instance;
-        private Player Player => GameManager.Player;
+        private bool _popUIFirstActionButtonAcknowledged;
+        private PopUI _currentPopUI;
+        private int _buildingActionButtonIndex;
 
+        private static DpadControl DPad => Gamepad.current.dpad;
+        private static GameManager GameManager => GameManager.Instance;
+        private static Player Player => GameManager.Player;
         private static InteractableObject[] InteractableObjectsOnMap =>
             FindObjectsByType<InteractableObject>(FindObjectsSortMode.None)
                 .Where(x => x.transform.Cast<Transform>().Any(child => child.gameObject.activeInHierarchy))
                 .ToArray();
+        private bool IsInBuilding => _currentPopUI != null;
+        private ActionButton CurrentPopUIButton => _currentPopUI.Buttons[_buildingActionButtonIndex];
+
 
         private void Awake()
         {
@@ -32,29 +41,57 @@ namespace Assets.Xbox
         {
             if (Gamepad.current == null || Player == null) return;
 
-            HandlePlayerMovement();
+            if (IsInBuilding)
+            {
+                HandlePlayerActions();
+            }
+            else
+            {
+                HandlePlayerMovement();
+            }
 
-            HandlePlayerActions();
+            HandleZoom();
+
+            if (_currentPopUI != null && !_popUIFirstActionButtonAcknowledged && _currentPopUI.Buttons.Any())
+            {
+                AcknowledgeFirstPopUIButton();
+            }
+        }
+
+        /// <summary>
+        /// Sets the current PopUI when entering/exiting a building to enable/disable movement controls and enable/disable action button cycling.
+        /// </summary>
+        /// <param name="popUI">The PopUI with the action buttons</param>
+        public void SetCurrentPopUI([CanBeNull] PopUI popUI)
+        {
+            _currentPopUI = popUI;
+            _popUIFirstActionButtonAcknowledged = false;
+            Debug.Log($"New Gameplay POP UI: {popUI?.name}");
+            if (_currentPopUI == null) return;
+
+            //I have this as separate for now in case I want to do something between the two if statements
+            if (!_currentPopUI.Buttons.Any()) return;
+
+            AcknowledgeFirstPopUIButton();
         }
 
         private void HandlePlayerMovement()
         {
-            var dPad = Gamepad.current.dpad;
             // Direction is rotated clockwise once because play facing direction is defined with Top left as up and bottom right as down
             // I guess we could support the option to have players choose.
-            if (dPad.up.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.RIGHT, out var rightMapTile))
+            if (DPad.up.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.RIGHT, out var rightMapTile))
             {
                 ProcessTileAction(rightMapTile);
             }
-            else if (dPad.right.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.DOWN, out var downMapTile))
+            else if (DPad.right.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.DOWN, out var downMapTile))
             {
                 ProcessTileAction(downMapTile);
             }
-            else if (dPad.down.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.LEFT, out var leftMapTile))
+            else if (DPad.down.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.LEFT, out var leftMapTile))
             {
                 ProcessTileAction(leftMapTile);
             }
-            else if (dPad.left.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.UP, out var upMapTile))
+            else if (DPad.left.wasPressedThisFrame && Player.AdjacentTiles.TryGetValue(PlayerFacingDirection.UP, out var upMapTile))
             {
                 ProcessTileAction(upMapTile);
             }
@@ -72,7 +109,66 @@ namespace Assets.Xbox
 
         private void HandlePlayerActions()
         {
+            if (DPad.left.wasPressedThisFrame)
+            {
+                HandleActionButtonNavigate(-1);
+            }
+            else if (DPad.right.wasPressedThisFrame)
+            {
+                HandleActionButtonNavigate(1);
+            }
 
+            //todo: Check if WORLD or EXIST button
+            if (Gamepad.current.buttonSouth.wasPressedThisFrame)
+            {
+                CurrentPopUIButton.HandleControllerExit();
+                if (CurrentPopUIButton.HasCriticalCircle)
+                {
+                    _currentPopUI.OnPointerDown(CurrentPopUIButton.ButtonName);
+                }
+                else
+                {
+                    _currentPopUI.OnClick(CurrentPopUIButton.ButtonName);
+                }
+            }
+            else if (Gamepad.current.buttonSouth.wasReleasedThisFrame)
+            {
+                CurrentPopUIButton.HandleControllerHover();
+                if (CurrentPopUIButton.HasCriticalCircle) _currentPopUI.OnPointerUp();
+            }
+
+            return;
+
+
+            void HandleActionButtonNavigate(int indexIncrement)
+            {
+                CurrentPopUIButton.HandleControllerExit();
+
+                //Add the increment but keep the index within the range of action button count using the mod factor
+                _buildingActionButtonIndex = (_buildingActionButtonIndex + indexIncrement + _currentPopUI.Buttons.Count) % _currentPopUI.Buttons.Count;
+
+                Debug.Log($"Current Pop Action is: {CurrentPopUIButton.ButtonName}");
+                CurrentPopUIButton.HandleControllerHover();
+            }
+        }
+
+        private void HandleZoom()
+        {
+            if (Gamepad.current.rightShoulder.wasPressedThisFrame || Gamepad.current.rightTrigger.wasPressedThisFrame)
+            {
+                GameControlsManager.TryZoom?.Invoke(1);
+            }
+            else if (Gamepad.current.leftShoulder.wasPressedThisFrame || Gamepad.current.leftTrigger.wasPressedThisFrame)
+            {
+                GameControlsManager.TryZoom?.Invoke(-1);
+            }
+        }
+
+        private void AcknowledgeFirstPopUIButton()
+        {
+            _popUIFirstActionButtonAcknowledged = true;
+            _buildingActionButtonIndex = 0;
+            CurrentPopUIButton.HandleControllerHover();
         }
     }
 }
--- Assets/_Scripts/ActionButton.cs
+++ Assets/_Scripts/ActionButton.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
@@ -23,7 +24,7 @@ public class ActionButton : MonoBehaviour
         yield return null;
         if (!ShouldWiggle || !Enabled || GameSettings.Instance.FTUE) yield break;
         transform.DOComplete();
-        transform.DOPunchScale(transform.localScale*0.5f, 0.5f, elasticity: 0f).SetDelay(WiggleDelay);
+        transform.DOPunchScale(transform.localScale * 0.5f, 0.5f, elasticity: 0f).SetDelay(WiggleDelay);
     }
 
     public void Wiggle(float delay = 0f)
@@ -31,6 +32,7 @@ public class ActionButton : MonoBehaviour
         transform.DOComplete();
         transform.DOPunchScale(transform.localScale * 0.5f, 0.5f, elasticity: 0f).SetDelay(delay);
     }
+
 
     public void RefreshButton(bool enabled)
     {
@@ -54,4 +56,34 @@ public class ActionButton : MonoBehaviour
     {
         Timer = timer;
     }
+
+    #region XboxSupport
+
+    private Vector3? _preHoverScale;
+
+
+    /// <summary>
+    /// Enlarge an action button to visibly show a user that it is the current action button that will be triggered by the controller
+    /// </summary>
+    public void HandleControllerHover()
+    {
+        transform.DOComplete();
+        _preHoverScale ??= transform.localScale;
+        transform.DOScale(_preHoverScale.Value * 1.5f, 0.5f);
+    }
+    /// <summary>
+    /// Reset an action button to visibly show a user that it is no longer the current action button that will be triggered by the controller
+    /// </summary>
+    public void HandleControllerExit()
+    {
+        transform.DOComplete();
+        transform.DOScale(_preHoverScale!.Value, 0.5f);
+        _preHoverScale = null;
+    }
+
+    public bool HasCriticalCircle => !ButtonName.Equals("WORLD", StringComparison.InvariantCultureIgnoreCase) &&
+                                     !ButtonName.Equals("ENTER", StringComparison.InvariantCultureIgnoreCase) &&
+                                     !ButtonName.Equals("EXIT", StringComparison.InvariantCultureIgnoreCase);
+
+    #endregion
 }
