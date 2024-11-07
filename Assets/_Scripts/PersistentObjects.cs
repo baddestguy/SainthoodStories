7605c6a4e793ed18d12b40aff1a8b6f68bea9cba
Eltee <lytaiwo@hotmail.com>
2024-11-06 21:08:29 -0600

    Fixed bug where game tried to set resolution on xbox.

--- Assets/_Scripts/PersistentObjects.cs
+++ Assets/_Scripts/PersistentObjects.cs
@@ -5,6 +5,7 @@ public class PersistentObjects : MonoBehaviour
 
     public static PersistentObjects instance;
     public bool developerMode;
+    private bool _hasInitialisedDependentSettings;
 
     private void Awake()
     {
@@ -16,7 +17,26 @@ public class PersistentObjects : MonoBehaviour
         Application.targetFrameRate = 60;
         DontDestroyOnLoad(gameObject);
 
+    }
+
+    private void Update()
+    {
+        if (_hasInitialisedDependentSettings) return;
+
+        if(GameSettings.Instance == null)
+        {
+            Debug.Log("Persistent Objects waiting for Game Settings");
+            return;
+        }
+
+        if (GameSettings.Instance.IsXboxMode)
+        {
+            _hasInitialisedDependentSettings = true;
+            return;
+        }
+
         //Safety check, Just in case resolution decides to be 0x0 (yes this can sometimes happen). This resolution is just for the splash screen, as it will be overridden once player hits Main Menu
         Screen.SetResolution(Screen.mainWindowDisplayInfo.width, Screen.mainWindowDisplayInfo.height, false);
+        _hasInitialisedDependentSettings = true;
     }
 }
