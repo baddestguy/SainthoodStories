6920c440023fe27f25c93abfd62da3385d743dd0
Eltee <lytaiwo@hotmail.com>
2024-11-06 21:20:23 -0600

    Occam's razor

--- Assets/_Scripts/PersistentObjects.cs
+++ Assets/_Scripts/PersistentObjects.cs
@@ -5,7 +5,6 @@ public class PersistentObjects : MonoBehaviour
 
     public static PersistentObjects instance;
     public bool developerMode;
-    private bool _hasInitialisedDependentSettings;
 
     private void Awake()
     {
@@ -17,26 +16,10 @@ public class PersistentObjects : MonoBehaviour
         Application.targetFrameRate = 60;
         DontDestroyOnLoad(gameObject);
 
-    }
-
-    private void Update()
-    {
-        if (_hasInitialisedDependentSettings) return;
-
-        if(GameSettings.Instance == null)
-        {
-            Debug.Log("Persistent Objects waiting for Game Settings");
-            return;
-        }
-
-        if (GameSettings.Instance.IsXboxMode)
-        {
-            _hasInitialisedDependentSettings = true;
-            return;
-        }
-
+#if !MICROSOFT_GDK_SUPPORT
         //Safety check, Just in case resolution decides to be 0x0 (yes this can sometimes happen). This resolution is just for the splash screen, as it will be overridden once player hits Main Menu
         Screen.SetResolution(Screen.mainWindowDisplayInfo.width, Screen.mainWindowDisplayInfo.height, false);
-        _hasInitialisedDependentSettings = true;
+#endif
+
     }
 }
